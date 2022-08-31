6079176a4bb5b5b15f6411d4d0826ac746970efe
shreyash <darjishreyash5@gmail.com>
2022-08-31 06:16:15 -0700

    Initial Commit

--- app/src/main/java/com/example/practical_2/MainActivity.kt
+++ app/src/main/java/com/example/practical_2/MainActivity.kt
@@ -8,6 +8,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(applicationContext, "onResume function called.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, "onCreate function called.", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "onCreate function called.", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(applicationContext, "onRestart function called.", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "onDestroy function called.", Toast.LENGTH_SHORT).show()
     }
 }
